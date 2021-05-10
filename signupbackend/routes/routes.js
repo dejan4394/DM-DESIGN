0059ffba3f60bd0cbabf6aa107f865fab1ddc33f
Your Name <dejanmalinov@yahoo.com>
2021-05-10 09:43:02 +0200

    Innitial Commit

--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sranje = require("../models/SignUpModels")
-
+const gomno = require("../models/SignInModels")
 
 router.post('/signup', (req, res)=>{
     const newUser = new sranje({
@@ -11,6 +11,22 @@ router.post('/signup', (req, res)=>{
         password : req.body.password
     })
     newUser.save()
+    .then(data=>{
+        res.json(data)
+    })
+    .catch(error=>{
+        res.json(error)
+    })
+});
+
+
+router.post('/signin', (req, res)=>{
+    const logedUser = new gomno({
+
+        email : req.body.email,
+        password : req.body.password
+    })
+    logedUser.save()
     .then(data=>{
         res.json(data)
     })
